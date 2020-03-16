006f11269b270d0dac4bd979f8c4322fd887400f
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-16 14:54:55 +0100

    main: little refactor and stuff
    - add new sha256 input
    - don't execute brew related stuff if GITHUB_ACTOR is not set
    - print arguments passed to `brew bump-formula-pr`
    - more and clearer error checking
    - clearer way of appending bump-formula-pr args

--- main.sh
+++ main.sh
@@ -7,20 +7,38 @@ actor="$GITHUB_ACTOR"
 token="$INPUT_TOKEN"
 formula="$INPUT_FORMULA"
 url="$INPUT_URL"
+sha256="$INPUT_SHA256"
 tag="$INPUT_TAG"
 revision="$INPUT_REVISION"
 force="$INPUT_FORCE"
 
-if test -n "$url" && test -n "$tag" && test -n "$revision"; then
-    echo "Don't specify 'url', 'tag' and 'revision' together."
-    echo "There are 2 options:"
-    echo "- specify only 'url'"
-    echo "- specify only 'tag' and 'revision'"
+if test -z "$formula"; then
+    echo "Need to specify the 'formula'"
+    exit 1
+fi
+
+if test -n "$url" && test -n "$tag"; then
+    echo "Can't specify 'url' and 'tag' together"
+    exit 1
+fi
+
+if test -n "$url" && test -n "$revision"; then
+    echo "Can't specify 'url' and 'revision' together"
+    exit 1
+fi
+
+if test -n "$tag" && test -n "$sha256"; then
+    echo "Can't specify 'tag' and 'sha256' together"
+    exit 1
+fi
+
+if test -n "$revision" && test -n "$sha256"; then
+    echo "Can't specify 'revision' and 'sha256' together"
     exit 1
 fi
 
 if test -z "$url" && test -z "$tag" && test -z "$revision"; then
-    echo "Need to specify 'url', or 'tag' and 'revision'"
+    echo "Need to specify 'url' with optional 'sha256', or 'tag' and 'revision'"
     exit 1
 fi
 
@@ -31,6 +49,34 @@ fi
 
 if test -z "$revision" && test -n "$tag"; then
     echo "Need to specify 'revision' with 'tag'"
+    exit 1
+fi
+
+args=(
+    --no-browse
+    --no-audit
+)
+
+if test -n "$force"; then
+    args+=(--force)
+fi
+if test -n "$url"; then
+    args+=(--url="$url")
+fi
+if test -n "$sha256"; then
+    args+=(--sha256="$sha256")
+fi
+if test -n "$tag"; then
+    args+=(--tag="$tag")
+fi
+if test -n "$revision"; then
+    args+=(--revision="$revision")
+fi
+
+echo "ARGS: ${args[@]}"
+
+if test -z "$actor"; then
+    echo "GITHUB_ACTOR not set"
     exit 1
 fi
 
@@ -45,21 +91,5 @@ git config --global user.email "$actor@users.noreply.github.com"
 git config --global user.name "$actor"
 
 export HOMEBREW_GITHUB_API_TOKEN="$token"
-
-args=(
-    --no-browse
-    --no-audit
-)
-
-if test -n "$force"; then
-    args+=(--force)
-fi
-
-if test -n "$url"; then
-    args+=(--url="$url")
-elif test -n "$tag" && test -n "$revision"; then
-    args+=(--tag="$tag")
-    args+=(--revision="$revision")
-fi
 
 brew bump-formula-pr "${args[@]}" "$formula"
