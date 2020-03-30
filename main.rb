833be540b9d6dff4b08c7c28b8aa61d9f2b98444
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-30 22:28:15 +0200

    main: fixes and improvements

--- main.rb
+++ main.rb
@@ -3,26 +3,25 @@
 require 'json'
 
 module Homebrew
-  def run(*cmd)
-    puts "[command]#{cmd.join(' ')}"
-    safe_system(*cmd)
-  end
+  module_function
 
-  def start_group(name)
-    puts "##[group]#{name}"
-  end
-
-  def end_group
-    puts '##[endgroup]'
+  def brew(*args)
+    puts "[command]brew #{args.join(' ')}"
+    safe_system('brew', *args)
   end
 
   # Get inputs
   token = ENV['INPUT_TOKEN']
   formula = ENV['INPUT_FORMULA']
-  tap = ENV['INPUT_TAP']
-  force = ENV['INPUT_FORCE']
   tag = ENV['INPUT_TAG']
   revision = ENV['INPUT_REVISION']
+  tap = ENV['INPUT_TAP']
+  force = ENV['INPUT_FORCE']
+
+  # Die if required inputs are not provided
+  odie "TOKEN is required" unless token
+  odie "FORMULA is required" unless formula
+  odie "TAG is required" unless tag
 
   # Set needed HOMEBREW environment variables
   ENV['HOMEBREW_GITHUB_API_TOKEN'] = token
@@ -32,41 +31,30 @@ module Homebrew
   ENV['GIT_COMMITTER_EMAIL'] = "#{ENV['GITHUB_ACTOR']}@users.noreply.github.com"
 
   # Update Homebrew
-  start_group('Update Homebrew')
-  run('brew', 'update-reset')
-  end_group
+  brew 'update-reset'
 
   # Tap if desired
   if tap
     formula = "#{tap}/#{formula}"
-    start_group('Tap repository')
-    run('brew', 'tap', tap)
-    end_group
+    brew 'tap', tap
   end
 
   # Get info about formula
-  info = JSON.parse(`brew info --json #{formula}`)
-  old_version = info.first['versions']['stable']
-  new_version = Version.parse(tag)
-  stable = info.first['urls']['stable']
-  using_git = stable['tag'] && stable['revision']
-  url = stable['url']
-  url.gsub!(old_version, new_version)
-  tag.delete_prefix!('refs/tags/')
+  stable = Formula[formula].stable
+  is_git = stable.downloader.is_a? GitDownloadStrategy
+
+  # Prepare tag and url
+  tag = tag.delete_prefix 'refs/tags/'
+  url = stable.url.gsub stable.version.to_s, Version.parse(tag).to_s
 
   # Finally bump the formula
-  start_group('Bump the formula')
-  run(
-    'brew',
-    'bump-formula-pr',
-    '--no-audit',
-    '--no-browse',
-    '--message=[`action-homebrew-bump-formula`](https://github.com/dawidd6/action-homebrew-bump-formula)',
-    *("--url=#{url}" unless using_git),
-    *("--tag=#{tag}" if using_git),
-    *("--revision=#{revision}" if using_git),
-    *('--force' if force),
-    formula
-  )
-  end_group
+  brew 'bump-formula-pr',
+       '--no-audit',
+       '--no-browse',
+       '--message=[`action-homebrew-bump-formula`](https://github.com/dawidd6/action-homebrew-bump-formula)',
+       *("--url=#{url}" unless is_git),
+       *("--tag=#{tag}" if is_git),
+       *("--revision=#{revision}" if is_git),
+       *('--force' if force),
+       formula
 end
