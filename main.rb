e538241cae52a7f319b23337454297c69662ff9d
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-01 16:36:24 +0200

    main: get info about user from API
    this way we can get actual full name and real email address of the actor

--- main.rb
+++ main.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'json'
-
 module Homebrew
   module_function
 
@@ -34,6 +32,24 @@ module Homebrew
   # Set needed HOMEBREW environment variables
   ENV['HOMEBREW_GITHUB_API_TOKEN'] = token
 
+  # Get user details
+  actor = ENV['GITHUB_ACTOR']
+  user = GitHub.open_api "#{GitHub::API_URL}/users/#{actor}"
+  user_name = user['name'] || actor
+  user_email = user['email'] || (
+    # https://help.github.com/en/github/setting-up-and-managing-your-github-user-account/setting-your-commit-email-address
+    user_created_at = Date.parse user['created_at']
+    plus_after_date = Date.parse '2017-07-18'
+    need_plus_email = (user_created_at - plus_after_date).positive?
+    user_email = "#{actor}@users.noreply.github.com"
+    user_email = "#{user['id']}+#{user_email}" if need_plus_email
+    user_email
+  )
+
+  # Tell git who you are
+  git 'config', '--global', 'user.name', user_name
+  git 'config', '--global', 'user.email', user_email
+
   # Update Homebrew
   brew 'update-reset'
 
@@ -47,11 +63,7 @@ module Homebrew
 
   # Prepare tag and url
   tag = tag.delete_prefix 'refs/tags/'
-  url = stable.url.gsub stable.version.to_s, Version.parse(tag).to_s
-
-  # Tell git who you are
-  git 'config', '--global', 'user.name', ENV['GITHUB_ACTOR']
-  git 'config', '--global', 'user.email', "#{ENV['GITHUB_ACTOR']}@users.noreply.github.com"
+  url = stable.url.gsub stable.version, Version.parse(tag)
 
   # Finally bump the formula
   brew 'bump-formula-pr',
