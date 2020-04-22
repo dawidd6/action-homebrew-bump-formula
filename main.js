b883ec53d800befe518f5a0ceacdbb0082fc684b
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-22 09:37:21 +0200

    main: find main ruby script in __dirname

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const exec = require('@actions/exec')
+const path = require('path')
 
 process.env.HOMEBREW_NO_ENV_FILTERING = "1"
 process.env.HOMEBREW_NO_AUTO_UPDATE = "1"
 process.env.HOMEBREW_NO_ANALYTICS = "1"
 process.env.HOMEBREW_COLOR = "1"
 
-exec.exec("brew", ["ruby", "main.rb"])
+exec.exec("brew", ["ruby", path.join(__dirname, "main.rb")])
     .catch(function () {
         process.exit(1)
     })
