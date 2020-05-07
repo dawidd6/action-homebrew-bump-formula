787ca6270ca80a504b01bd6daff1e69e763fd7b8
Dawid Dziurla <dawidd0811@gmail.com>
2020-05-07 11:59:47 +0200

    main: better readability

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const exec = require('@actions/exec')
+const core = require('@actions/core')
 const path = require('path')
 
-process.env.HOMEBREW_NO_ENV_FILTERING = "1"
-process.env.HOMEBREW_NO_AUTO_UPDATE = "1"
-process.env.HOMEBREW_NO_ANALYTICS = "1"
-process.env.HOMEBREW_COLOR = "1"
+async function main() {
+    try {
+        process.env.HOMEBREW_NO_ENV_FILTERING = "1"
+        process.env.HOMEBREW_NO_AUTO_UPDATE = "1"
+        process.env.HOMEBREW_NO_ANALYTICS = "1"
+        process.env.HOMEBREW_COLOR = "1"
 
-exec.exec("brew", ["ruby", path.join(__dirname, "main.rb")])
-    .catch(function (err) {
-        console.log(err)
-        process.exit(1)
-    })
+        await exec.exec("brew", ["ruby", path.join(__dirname, "main.rb")])
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+main()
