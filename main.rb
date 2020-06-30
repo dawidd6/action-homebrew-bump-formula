63021a94a27f51f5b995fe594100798b8f0c6fb8
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-30 12:08:21 +0200

    main: fix user email

--- main.rb
+++ main.rb
@@ -66,13 +66,14 @@ module Homebrew
   # Get user details
   user = GitHub.open_api "#{GitHub::API_URL}/user"
   user_id = user['id']
+  user_login = user['login']
   user_name = user['name'] || user['login']
   user_email = user['email'] || (
     # https://help.github.com/en/github/setting-up-and-managing-your-github-user-account/setting-your-commit-email-address
     user_created_at = Date.parse user['created_at']
     plus_after_date = Date.parse '2017-07-18'
     need_plus_email = (user_created_at - plus_after_date).positive?
-    user_email = "#{user_name}@users.noreply.github.com"
+    user_email = "#{user_login}@users.noreply.github.com"
     user_email = "#{user_id}+#{user_email}" if need_plus_email
     user_email
   )
