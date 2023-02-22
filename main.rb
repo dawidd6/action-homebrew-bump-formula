e9b43cd30eec6ea80777e7e22e1526beb1675c18
Dawid Dziurla <dawidd0811@gmail.com>
2023-02-22 12:53:48 +0100

    main: always tap homebrew/core
    for now at least

--- main.rb
+++ main.rb
@@ -73,12 +73,11 @@ module Homebrew
   git 'config', '--global', 'user.name', user_name
   git 'config', '--global', 'user.email', user_email
 
-  # Tap the correct tap
-  if tap.blank?
-    brew 'tap', 'homebrew/core'
-  else
-    brew 'tap', tap
-  end
+  # Always tap homebrew/core, otherwise brew can't find formulae
+  brew 'tap', 'homebrew/core'
+  
+  # Tap the requested tap if applicable
+  brew 'tap', tap unless tap.blank?
 
   # Append additional PR message
   message = if message.blank?
