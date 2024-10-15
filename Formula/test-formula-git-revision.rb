858f7a7260afb0cca648fe35a62e5c9aef7c07f2
Dawid Dziurla <dawidd6@users.noreply.github.com>
2024-10-15 10:26:19 +0000

    test-formula-git-revision 0.7.0

--- Formula/test-formula-git-revision.rb
+++ Formula/test-formula-git-revision.rb
@@ -2,8 +2,8 @@ class TestFormulaGitRevision < Formula
   desc "Formula to test Action"
   homepage "https://github.com/Debian/dh-make-golang"
   url "https://github.com/Debian/dh-make-golang.git",
-    tag:      "v0.3.2",
-    revision: "82916c0d56b6319398f635199222dff397fafc12"
+    tag:      "v0.7.0",
+    revision: "ba882c9bfa490f59921a3e7b514009ef05097e56"
   license "MIT"
   head "https://github.com/Debian/dh-make-golang.git"
 
