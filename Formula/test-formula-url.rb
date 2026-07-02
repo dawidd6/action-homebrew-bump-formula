e1ee9cceb1a474772355a075327f5822e818ea1e
Dawid Dziurla <dawidd6@users.noreply.github.com>
2026-07-02 11:34:05 +0000

    test-formula-url 0.1.15

--- Formula/test-formula-url.rb
+++ Formula/test-formula-url.rb
@@ -1,8 +1,8 @@
 class TestFormulaUrl < Formula
   desc "Formula to test Action"
   homepage "https://github.com/dawidd6/actions-updater"
-  url "https://github.com/dawidd6/actions-updater/archive/v0.1.11.tar.gz"
-  sha256 "b1c83ee9d19289eb403ad0863c235fa9c3b3a980c9b13a43cda9fc9413935df4"
+  url "https://github.com/dawidd6/actions-updater/archive/v0.1.15.tar.gz"
+  sha256 "e415ab6bb8547d2571d1dd80c686531d21f191e8f080f351355891b1bf96075f"
   license "MIT"
 
   def install
